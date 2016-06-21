a3db6e490013ca975ab0c3418020603bae4b887c
Thiago Vieira <thiagovieiraas@gmail.com>
2016-06-21 12:54:09 -0300

    Removed the iterative boruvka file and added a test generator

---
+++ tst/genTest_completeGraph.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+# 
+# This script serves to create test cases
+#  with complete graphs.
+#
+
+import sys
+from random import randint
+
+
+def genComplete(n):
+  m = n*(n-1)/2
+  print str(n) + ' ' + str(m)
+  for i in range(1, n+1):
+    for j in range(i+1, n+1):
+      print str(i) + ' ' + str(j) + ' ' + str(randint(1, 100))
+  
+
+
+def main():
+  if len(sys.argv) != 2:
+    print 'Error: Invalid arguments'
+    print 'Usage: ./' + str(sys.argv[0]) + ' [Number of nodes the complete graph will have]'
+    return
+
+  n = int(sys.argv[1])
+  genComplete(n)
+
+
+if __name__ == '__main__':
+  main()
